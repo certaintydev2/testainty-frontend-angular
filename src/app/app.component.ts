c32dcca337298fbf1c62337e510780f9b1250ac3
certaintydev2 <79897663+certaintydev2@users.noreply.github.com>
2022-12-28 10:36:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { LoginService } from './services/login/login.service';
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
   title = 'InterviewTest';
+
+  constructor(private _router: Router, public _loginServcie: LoginService) { }
+
+
+  ngOnInit() {
+
+    // if (this._loginServcie.getToken()?.length)
+    //   this._router.navigateByUrl('pages/candidate-profile')
+    // else
+    //   this._router.navigateByUrl('login')
+  }
+
+
+
+  ngOnDestroy() {
+  }
+
+
 }
